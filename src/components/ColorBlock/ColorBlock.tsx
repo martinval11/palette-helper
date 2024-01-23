e77c2331f8e9bed80366221c76c01557663c0b8f
martínval11 <martinvaldez.dev@gmail.com>
2024-01-23 12:26:58 -0300

    Lock & Unlock color fix

--- src/components/ColorBlock/ColorBlock.tsx
+++ src/components/ColorBlock/ColorBlock.tsx
@@ -1,17 +1,33 @@
-import { useState } from 'preact/hooks';
+import { useState, type StateUpdater, useEffect } from 'preact/hooks';
 import styles from './colorblock.module.css';
 
 export function ColorBlock({ color }: { color: string }) {
+  const [savedColor, setSavedColor] = useState('')
+  const [colorToRender, setColorToRender] = useState(color);
   const [locked, setLocked] = useState(false);
+
+  useEffect(() => {
+    setColorToRender(color)
+  }, [color])
 
   return (
     <div
       className={styles.colorBlock}
-      style={{ background: `${color} ${locked ? '!important' : ''}` }}
+      style={{ background: `${locked ? savedColor : colorToRender}` }}
     >
       <div className={styles.blockContent}>
-        <h3>{color}</h3>
-        <button onClick={() => setLocked(!locked)}>
+        <h3>{locked ? savedColor : colorToRender}</h3>
+        <button onClick={() => {
+          if (locked) {
+            setColorToRender(savedColor)
+          }
+          
+          if (!locked) {
+            setSavedColor(color);
+          }
+
+          setLocked(!locked)
+        }}>
           {locked ? 'Unlock' : 'Lock'}
         </button>
       </div>
