f771c4e058b12e74619d274a7cc1fe65b3db8f4e
martínval11 <martinvaldez.dev@gmail.com>
2024-01-23 12:37:41 -0300

    contrast fix with light colors

--- src/components/RandomPalette/RandomPalette.tsx
+++ src/components/RandomPalette/RandomPalette.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'preact/hooks';
+import { useEffect, useState } from 'preact/hooks';
 import { generateColor } from '../../lib/generateColor';
 
 import { ColorBlock } from '../ColorBlock/ColorBlock';
--- src/lib/generateColor.ts
+++ src/lib/generateColor.ts
@@ -2,7 +2,6 @@ import { randomColor } from './randomColor';
 
 export function generateColor() {
   return {
-    id: 1,
     color: randomColor(),
   };
 }
